afd9fd2b404ea8faa8097932adc7ff176d10d11f
Jiří Třečák <jiritrecak@gmail.com>
2016-09-23 16:30:15 +0200

    Fixed example alamofire requests to properly download contributors

--- Example/Laurine/Classes/Model/API/ContributorAPI.swift
+++ Example/Laurine/Classes/Model/API/ContributorAPI.swift
@@ -44,42 +44,40 @@ class ContributorAPI {
     
     func getContributors(_ handler : @escaping (_ contributors : [Contributor]?, _ error : NSError?) -> ()) {
         
-//        Alamofire.request(BASE_URL_CONTRIBUTORS, method: .get, parameters: nil, encoding: .JSONEncoding.default)
-//            .responseJSON { response in
-//                
-//                if let JSON = response.result.value as? [NSDictionary] {
-//                    
-//                    // Create contributors
-//                    var output : [Contributor] = []
-//                    JSON.forEach({ (data) -> () in
-//                        output.append(Contributor(fromDictionary: data))
-//                    })
-//                    
-//                    // Notify caller
-//                    handler(contributors: output, error: nil)
-//                } else {
-//                    handler(contributors: nil, error: response.result.error)
-//                }
-//        }
+        Alamofire.request(BASE_URL_CONTRIBUTORS, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response:DataResponse<Any>) in
+            if let JSON = response.result.value as? [NSDictionary] {
+                
+                // Create contributors
+                var output : [Contributor] = []
+                JSON.forEach({ (data) -> () in
+                    output.append(Contributor(fromDictionary: data))
+                })
+                
+                // Notify caller
+                handler(output, nil)
+            } else {
+                handler(nil, response.result.error as NSError?)
+            }
+        }
     }
     
     
     func updateContributor(_ contributor : Contributor, handler : @escaping (_ contributor : Contributor?, _ error : NSError?) -> ()) {
         
-//        Alamofire.request(contributor.detailURL, method: .get, parameters: nil, encoding: .JSONEncoding.default)
-//            .responseJSON { response in
-//                
-//                if let JSON = response.result.value as? NSDictionary {
-//                    
-//                    // Update contributor
-//                    contributor.updateWithDictionary(JSON)
-//                    
-//                    // Notify caller
-//                    handler(contributor: contributor, error: nil)
-//                } else {
-//                    handler(contributor: nil, error: response.result.error)
-//                }
-//        }
+        Alamofire.request(contributor.detailURL, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response:DataResponse<Any>) in
+            
+            if let JSON = response.result.value as? NSDictionary {
+                
+                // Update contributor
+                if contributor.updateWithDictionary(JSON) {
+                    
+                    // Notify caller
+                    handler(contributor, nil)
+                }
+            } else {
+                handler(nil, response.result.error as? NSError)
+            }
+        }
     }
 }
 
